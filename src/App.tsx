2fed71b03ba455b91b950f467f8a015f3a95a003
ilsaf-jpg <62942490+ilsaf-jpg@users.noreply.github.com>
2021-05-30 18:40:00 +0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react"
 import "./App.css"
 import { ChartsPage } from "./components/Charts/ChartsPage"
-import "bootstrap/dist/css/bootstrap.min.css"
 import TradingViewWidget from "react-tradingview-widget"
 function App() {
   return (
     <div>
+      <ChartsPage />
       <TradingViewWidget symbol="NASDAQ:AAPL" />
-      {/* <ChartsPage /> */}
     </div>
   )
 }
